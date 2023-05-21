df84b1cf3364d5cde73b061b241f800c35522b94
xiaoleGun <1592501605@qq.com>
2023-05-21 17:26:18 +0800

    wayne: Bring back to old camera
    Change-Id: If4dacc698347ee678fbae2a2f1e925e13af7a6c3
    Signed-off-by: xiaoleGun <1592501605@qq.com>

--- device.mk
+++ device.mk
@@ -95,8 +95,8 @@ PRODUCT_PACKAGES += \
     android.hardware.camera.device@3.5:64 \
     android.hardware.camera.provider@2.4-impl:32 \
     android.hardware.camera.provider@2.4-service \
+    android.hardware.camera.provider@2.5:64 \
     android.hardware.camera.provider@2.6:64 \
-    camera.sdm660 \
     libdng_sdk.vendor \
     vendor.qti.hardware.camera.device@1.0:64
 
