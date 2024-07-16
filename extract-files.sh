bdaaeab837c84b91332fff56ec6208f8474ccf81
xiaoleGun <1592501605@qq.com>
2024-07-16 09:28:55 +0800

    wayne: Also patch fingerprint blobs to use v32
    Change-Id: I32b05e94e0b4b821c404df3cb1522896b82e0f2c
    Signed-off-by: xiaoleGun <1592501605@qq.com>

--- extract-files.sh
+++ extract-files.sh
@@ -62,7 +62,7 @@ function blob_fixup() {
             "${PATCHELF}" --remove-needed vendor.xiaomi.hardware.mtdservice@1.0.so "${2}"
             sed -i "s|/system/etc/firmware|/vendor/firmware\x0\x0\x0\x0|g" "${2}"
             ;;
-        vendor/lib64/vendor.xiaomi.hardware.mlipay@1.1.so | vendor/lib64/vendor.xiaomi.hardware.mlipay@1.0.so)
+        vendor/lib64/vendor.xiaomi.hardware.mlipay@1.1.so | vendor/lib64/vendor.xiaomi.hardware.mlipay@1.0.so | vendor/lib64/libvendor.goodix.hardware.fingerprint@1.0.so | vendor/lib64/com.fingerprints.extension@1.0.so)
             "${PATCHELF}" --replace-needed "libhidlbase.so" "libhidlbase-v32.so" "${2}"
             ;;
     esac
